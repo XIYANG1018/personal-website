78b0703a76ca5953e2fcbbb04bbcdf68ba281039
XIYANG1018 <pkuyangxi@163.com>
2024-06-08 00:05:25 -0700

    change bg color

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ import AboutSection from "./components/AboutSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-custom-yellow">
+    <main className="flex min-h-screen flex-col bg-[#eef]">
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
       
